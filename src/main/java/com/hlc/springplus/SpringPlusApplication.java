98e04b635e1eff92ed8513abdce01353d0319710
spring <huanglinchun@runjingchina.com>
2024-02-04 14:29:24 +0800

    初始化

---
+++ src/main/java/com/hlc/springplus/SpringPlusApplication.java
@@ -0,0 +1,13 @@
+package com.hlc.springplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hlc/springplus/SpringPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlc.springplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
